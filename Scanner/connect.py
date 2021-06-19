e285868dd3476b3ad16b9de493d2f5607bbacc7a
jaikeex <74075440+jaikeex@users.noreply.github.com>
2021-06-19 13:25:20 +0200

    Update connect.py
    Added documentation

--- Scanner/connect.py
+++ Scanner/connect.py
@@ -4,6 +4,12 @@ import datetime
 
 
 def dantem_login(username: str, password: str) -> requests.Session:
+    """
+    Creates a session and posts login info to the server.
+    :param username: login username
+    :param password: user password
+    :return: session to use for scanning the marketplace
+    """
     session = requests.Session()
     url = "https://dap.dantem.net/auth/login/"
     payload = {"username": username,
@@ -19,7 +25,18 @@ def scan_and_accept(session: requests.Session,
                     from_date: str,
                     to_date: str,
                     dates: list,
-                    accept: bool):
+                    accept: bool): -> str:
+    """
+    Function encompassing the entire scan process.
+    Scans the market for new shifts and then either accepts or reports it
+    based on arguments.
+    :param session: requests.session to use for the scan
+    :param from_date: first date to scan
+    :param to_date: last date to scan
+    :param dates: time availability of the user for accepting shifts
+    :param accept: boolean to indicate wheter to autoaccept shifts
+    :return: string with info about the results
+    """
     now = datetime.datetime.today().strftime('%H:%M:%S')
     scan_response = scan_market(session=session,
                                 from_date=from_date,
@@ -42,7 +59,10 @@ def scan_and_accept(session: requests.Session,
 def scan_and_report(session: requests.Session,
                     from_date: str,
                     to_date: str,
-                    dates: list):
+                    dates: list) -> str:
+    """
+    Not used for now.
+    """
     now = datetime.datetime.today().strftime('%H:%M:%S')
     scan_response = scan_market(session=session,
                                 from_date=from_date,
@@ -64,6 +84,13 @@ def scan_and_report(session: requests.Session,
 def scan_market(session: requests.Session,
                 from_date: str,
                 to_date: str) -> requests.models.Response.text:
+    """
+    Connects to the marketplace and retrieves its content.
+    :param session: requests.session to use for the scan
+    :param from_date: first date to scan
+    :param to_date: last date to scan
+    :return: response text of the marketplace website
+    """
     market_form = {"fromDate":      from_date,
                    "toDate":        to_date,
                    "type":          "all",
@@ -80,11 +107,21 @@ def scan_market(session: requests.Session,
 
 
 def is_empty(response: requests.models.Response.text) -> bool:
+    """
+    Checks wheter there are new shifts available on the market.
+    :param response: response text of the market website
+    :return: True if empty, False otherwise
+    """
     no_offer_text = "Aktuálnímu filtru neodpovídají žádné nabídky"
     return no_offer_text in response
 
 
 def parse_market(response: requests.models.Response.text) -> list:
+    """
+    Utilizes BeautifulSoup to parse the market website and organize its content.
+    :param response: response text of the market website
+    :return: list conatining shift info as dictionaries
+    """
     soup = BeautifulSoup(response, "html.parser")
     body = soup.findAll("body")[0]
     divs = body.findAll("div")
@@ -116,6 +153,11 @@ def parse_market(response: requests.models.Response.text) -> list:
 
 
 def get_datetime(shift: dict) -> datetime.datetime:
+    """
+    Converts a string of shift start time into a python datetime.
+    :param shift: dict with shift info
+    :return: datetime object of shift start time
+    """
     date_time = shift["datetime"]
     dt = datetime.datetime.strptime(date_time, "%d.%m.%Y %H:%M")
     return dt
@@ -124,11 +166,24 @@ def get_datetime(shift: dict) -> datetime.datetime:
 def is_in_datetime_range(start: datetime.datetime,
                          end: datetime.datetime,
                          time: datetime.datetime) -> bool:
+    """
+    Checks whether a datetime is in a given datetime interval.
+    :param start: start of the interval to check against
+    :param end: end of the interval to check against
+    :param time: datetime to check
+    :return: True if in interval, False otherwise
+    """
     return start <= time <= end
 
 
 def is_valid_offer(shift: dict,
                    dates: list) -> bool:
+    """
+    Checks whether the offer is valid based on its date and time availability.
+    :param shift: dict with shift info
+    :param dates: time availability of the user
+    :return: True if the shift is valid, False otherwise
+    """
     shift_time = get_datetime(shift)
     is_valid = False
     for date in dates:
@@ -140,11 +195,22 @@ def is_valid_offer(shift: dict,
 
 
 def accept_offer(session: requests.Session, shift: dict) -> None:
+    """
+    Accepts the shift offer.
+    :param session: active session of the marketplace
+    :param shift: dict with shift info
+    :return: None
+    """
     link = shift["link"]
     session.post(link)
 
 
 def shift_to_string(shift: dict) -> str:
+    """
+    Converts a shift dict to a string.
+    :param shift: dict with shift info
+    :return: string with shift info
+    """
     shift_string = ""
     shift_string += shift["datetime"]
     shift_string += ",  "
